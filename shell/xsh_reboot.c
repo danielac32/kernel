e33d2640d2072cbe10da18f48cf0275fa46523b5
danielac32 <danielquinteroac32@gmail.com>
2024-05-04 17:23:22 -0400

    first commit

---
+++ shell/xsh_reboot.c
@@ -0,0 +1,19 @@
+/* xsh_echo.c - xsh_echo */
+
+#include <xinu.h>
+#include <stdio.h>
+
+
+
+/*------------------------------------------------------------------------
+ * xhs_echo - write argument strings to stdout
+ *------------------------------------------------------------------------
+ */
+shellcmd xsh_reboot(int nargs, char *args[])
+{
+	//int32	i;			/* walks through args array	*/
+
+	 
+
+	return 0;
+}
